0f73911059c9dee6602ebeb1cabaf4d699c5a9e6
Simsaris <4coleh10@solent.ac.uk>
2019-10-09 08:52:19 +0000

    debugging for final commit

--- 1-basics/4-repetition/1-while-loop/1-simple/bot.py
+++ 1-basics/4-repetition/1-while-loop/1-simple/bot.py
@@ -2,6 +2,6 @@ print("how many cables should i remove?")
 cables = int(input())
 removed = 0
 
-while (removed <= cables):
+while (removed < cables):
+    removed +=1
     print("cable removed")
-    removed+=1
