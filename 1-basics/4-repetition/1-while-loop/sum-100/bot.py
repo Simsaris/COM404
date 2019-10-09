241e75e88f280b75c19587f1d71c44e6027ba654
Simsaris <4coleh10@solent.ac.uk>
2019-10-09 10:09:37 +0000

    activity 5 sum 100 complete

---
+++ 1-basics/4-repetition/1-while-loop/sum-100/bot.py
@@ -0,0 +1,9 @@
+print("calculate the sum of the first 100 numbers")
+count = 1
+total = 0
+
+while count <= 100:
+    total+=count #total=total+count#
+    count+=1
+
+print("the total is " + str(total))
