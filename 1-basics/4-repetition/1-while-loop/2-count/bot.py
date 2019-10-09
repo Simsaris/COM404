4c8be7f5a9253a7613fe3e884e6b18c1c62c3bca
Simsaris <4coleh10@solent.ac.uk>
2019-10-09 09:03:48 +0000

    added a final print statement to indicate completion

--- 1-basics/4-repetition/1-while-loop/2-count/bot.py
+++ 1-basics/4-repetition/1-while-loop/2-count/bot.py
@@ -6,3 +6,6 @@ while (LiveWireAvoided < LiveWireInput):
     LiveWireAvoided +=1
     print("Avoiding..." )
     print("Done! " +str(LiveWireAvoided) + " wire(s) have been avoided!")
+
+if (LiveWireAvoided == LiveWireInput):
+    print("All wires have have been avoided!")
